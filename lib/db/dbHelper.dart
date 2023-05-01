cdc9fc4c6cd13e2bb6fc2fb710ba74ffa7a48f33
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-05-01 14:38:30 +0900

    0501_14:38 changed dbHelper.dart

--- lib/db/dbHelper.dart
+++ lib/db/dbHelper.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import '../model/Book.dart';
 
 class dbHelper {
   late Database? _database;
@@ -59,8 +60,12 @@ class dbHelper {
     )
   }
 
-  // Access to Database (CRUD)
-  // get All from Database (SELECT *)
+  /* Access to Database (CRUD)
+      With Sqflite
+  */
+  // Book Start
+
+    // get All from Database (SELECT *)
   Future<List<Book>> getAllBook() async {
     final db = await database;
     final List<Map<String, dynamic>> maps = await db!.query('Book');
@@ -72,9 +77,32 @@ class dbHelper {
     return list;
   }
 
-  // insert
+  // get books with Word
+  Future<List<Book>> getBookFromW(String w, String targetArgs) async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db?.query('Book', where: w + ' = ?', whereArgs: [targetArgs]);
+    if (maps.isEmpty) return [];
+    List<Book> list = List.generate(maps.length, (index) => {
+      return Book.fromMap(maps[index]);
+    });
+    return list;
+  }
+
+    // insert
   Future<void> insert(Book book) async {
     final db = await database;
     book.Seq = await db?.insert('Book', book.toMap());
   }
+
+  Future<void> update(Book book) async {
+    final db = await database;
+    await db?.update('Book', book.toMap(), where: "SEQ = ?", whereArgs: [book.Seq]);
+  }
+
+  Future<void> delete(Book book) async {
+    final db = await database;
+    await db?.delete('Book', where: "SEQ = ?", whereArgs: [book.Seq]);
+  }
+
+  // Book end
 }
